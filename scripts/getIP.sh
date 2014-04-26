3ff6b11c6ac156356e8c233516b5ff55d622300d
nadyawilliams <nadya@sdsc.edu>
2014-04-25 18:02:36 -0700

    use 2 args

--- scripts/getIP.sh
+++ scripts/getIP.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# Parse apache logs for a given year and collect uniq hostips
+# Parse apache logs for a given year/month and collect uniq hostips
 # from which there was a request for web service. 
-# create output file of uniq hostips for a year. 
+# create output file of uniq hostips for a year/moth. 
+
+if [ $# -eq 2 ]; then
+    YEAR=$1
+    MONTH=$2
+else
+    # show help
+    echo "Usage: `basename $0` year month"
+    echo "Parse apache logs for a given year and month. Output file is ips-\$year-\$month"
+    echo "       year - 4 digit year. For example:  2013"
+    echo "       month - 2 digit month. For example: 01 for Jan, 02 for Feb, etc."
+    exit
+fi
 
 # apache logs location
-YEAR=2013
-LOGDIR=/share/backup/apache_logs/$YEAR*
+LOGDIR=/share/backup/apache_logs/$YEAR$MONTH*
 files=`ls $LOGDIR/access_log*`
 
 # output file
-OUT=ips$YEAR
+OUT=ips-$YEAR-$MONTH
+TEMP=ips-$YEAR-$MONTH.temp
 
-touch $OUT
+touch $TEMP
 for i in $files
 do
-    grep POST $i | awk '{print $1}' | sort | uniq >> $OUT
+    grep POST $i | awk '{print $1}' | sort | uniq >> $TEMP
 done
 
-sort $OUT | uniq > $OUT.uniq
+sort $TEMP | uniq > $OUT
+rm -rf $TEMP
